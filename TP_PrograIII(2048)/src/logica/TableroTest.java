07b70d40e00badf1a5a648d143cf6f51291405c6
nicolas-palacio <nico.p_2011@live.com.ar>
2021-03-27 13:05:00 -0300

    Casos de test añadidos:moverDerecha moverIzquierda

--- TP_PrograIII(2048)/src/logica/TableroTest.java
+++ TP_PrograIII(2048)/src/logica/TableroTest.java
@@ -2,13 +2,50 @@ package logica;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TableroTest {
-
-	@Test
+	private Tablero tablero,tableroAComparar;
+	
+	@Before
 	public void test() {
-		fail("Not yet implemented");
+		int[][] matriz= { {0,2,2,4},
+				{2,4,8,4},
+				{32,32,32,2},
+				{8,4,4,8}
+		};
+		tablero=new Tablero(matriz);
+	}
+	
+	@Test
+	public void moverDerechaTest() {
+		 tableroAComparar=new Tablero(new int[][]{ 
+			{0,0,4,4},
+			{2,4,8,4},
+			{0,32,64,2},
+			{0,8,8,8}
+		});
+		
+		tablero.movimientoUsuario("derecha");
+		
+
+		assertEquals(tableroAComparar.toString(),tablero.toString());
+	}
+	
+	@Test
+	public void moverIzquierdaTest() {
+		 tableroAComparar=new Tablero(new int[][]{ 
+			{4,4,0,0},
+			{2,4,8,4},
+			{64,32,2,0},
+			{8,8,8,0}
+		});
+		
+		tablero.movimientoUsuario("izquierda");
+		
+
+		assertEquals(tableroAComparar.toString(),tablero.toString());
 	}
 
 }
