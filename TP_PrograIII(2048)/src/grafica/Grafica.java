b1d11581c0c2f6df81c1b08a3ad43118316de592
FigasaCHAN <FigasaCHAN@DESKTOP-FCGEU73>
2021-03-30 11:05:46 -0300

    Diseño de la parte grafica, utilizamos JPanel como containers

--- TP_PrograIII(2048)/src/grafica/Grafica.java
+++ TP_PrograIII(2048)/src/grafica/Grafica.java
@@ -3,6 +3,12 @@ package grafica;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class Grafica {
 
@@ -40,6 +46,21 @@ public class Grafica {
 		frame.setTitle("2048");
 		frame.setBounds(100, 100, 800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel hub = new JPanel();
+		hub.setBounds(0, 0, 784, 35);
+		frame.getContentPane().add(hub);
+		hub.setLayout(null);
+		
+		JLabel puntaje = new JLabel("Puntos: ");
+		puntaje.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		puntaje.setBounds(513, 0, 271, 35);
+		hub.add(puntaje);
+		
+		JPanel tableroGrafico = new JPanel();
+		tableroGrafico.setBounds(0, 35, 784, 526);
+		frame.getContentPane().add(tableroGrafico);
+		tableroGrafico.setLayout(new GridLayout(1, 0, 0, 0));
 	}
-
 }
