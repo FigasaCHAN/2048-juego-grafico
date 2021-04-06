972f509b7f60a9c1202cdd3440a98ebdeca5dc0f
nicolas-palacio <nico.p_2011@live.com.ar>
2021-04-06 16:24:38 -0300

    Se agrego la clase MejoresJugadoresPanel .

---
+++ TP_PrograIII(2048)/src/grafica/MejoresJugadoresPanel.java
@@ -0,0 +1,14 @@
+package grafica;
+
+import javax.swing.JPanel;
+
+public class MejoresJugadoresPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public MejoresJugadoresPanel() {
+
+	}
+
+}
