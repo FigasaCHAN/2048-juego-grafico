4f585f1c64aae3f07da467079587a3a2a48174af
nicolas-palacio <nico.p_2011@live.com.ar>
2021-04-09 17:52:08 -0300

    En Menu.java se cambio nombre de la funcion boolean privada.

--- TP_PrograIII(2048)/src/logica/Menu.java
+++ TP_PrograIII(2048)/src/logica/Menu.java
@@ -5,7 +5,7 @@ import java.util.stream.IntStream;
 
 public class Menu {
 	
-	private static boolean contieneNumeros(String nombre) {
+	private static boolean tieneCaracteresNoValidos(String nombre) {
 		
 		for(char caracter:nombre.toCharArray()) {
 		   if(!Character.isLetter(caracter) || Character.isDigit(caracter)) {
@@ -17,7 +17,7 @@ public class Menu {
 	
 	public static boolean validarNombreJugador(String nombre) {
 		
-		if(nombre.contains(" ") || contieneNumeros(nombre) || nombre.isEmpty()) {
+		if(nombre.contains(" ") || tieneCaracteresNoValidos(nombre) || nombre.isEmpty()) {
 			return false;
 		}else {		
 			return true;
